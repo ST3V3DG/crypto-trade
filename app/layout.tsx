8305b2847fae5da494f58ebddac93809274cde87
ST3V3DG <rosycrystal024@gmail.com>
2025-12-17 04:36:57 +0100

    add @vercel/analytics dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html className="dark" lang="en">
-			<body className={`${khand.className} antialiased`}>{children}</body>
+			<body className={`${khand.className} antialiased`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
